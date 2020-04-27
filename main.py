95a73deedbeb31baf6d020b5d2c2b3490de4aa50
Christian <gasconc@gmail.com>
2020-04-27 12:27:05 -0500

    adding output file with pandas

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from mercadoPago import create_customer, tokenize, save_card, set_default_card
 import json
 import logging
 import xlrd
+import requests
+import xlwt
+from xlwt import Workbook
+import pandas as pd
 
 #logging definitions
 LOG_FILENAME = 'logs.log'
@@ -13,12 +17,19 @@ loc = ("Data.xlsx")
 wb = xlrd.open_workbook(loc) 
 sheet = wb.sheet_by_index(0)
 
+# Output file
+wb = Workbook()  
+sheet1 = wb.add_sheet('Sheet 1')
+
 #Printing output definition
 class bcolors:
     OKGREEN = '\033[92m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
-    
+#initialization
+bodytech_ids=[]
+customer_ids=[]
+card_ids=[]
 
 if __name__ == "__main__":
     
@@ -65,16 +76,28 @@ if __name__ == "__main__":
             #logging 
             
             logging.info('Customer creado con éxito: ' +str(output) )
+            customer_ids.append(output[1])
+            bodytech_ids.append(output[0])
+            card_ids.append(output[3])
+            print(customer_ids)
+            print(card_ids)
 
-        except Exception as e: 
+        except requests.exceptions.HTTPError as err:
             #handling exceptions
-            print(bcolors.FAIL + ' Error en la creación del usuario ' +  ' ERROR: ' + str(e) + ' - columna: ' + str(i))
-            print(bcolors.WARNING + '>> Progreso ' + str(int((i / total_users) *100 ))+'% >> Success rate:' + str(int((exito / i)*100 ))+ '%')
-            logging.error ('Customer ha fallado en su creación: ' +str(output) + ' ERROR: ' + str(e) + ' - columna: ' + str(i))
+            print(err)
+            #print(bcolors.FAIL + ' Error en la creación del usuario ' +  ' ERROR: ' + str(e) + ' - columna: ' + str(i))
+            #print(bcolors.WARNING + '>> Progreso ' + str(int((i / total_users) *100 ))+'% >> Success rate:' + str(int((exito / i)*100 ))+ '%')
+            logging.error ('Customer ha fallado en su creación: ' +str(output) + ' ERROR: ' + str(err) + ' - columna: ' + str(i))
             fail.append(str(user['metadata']['id']))
 
     #logging final metrics
+    wb.save('output.xls') 
     print(bcolors.WARNING + 'Proceso finalizado >> Success rate:' + str(int((exito / i)*100) )+ '%')
     logging.info('Proceso finalizado >> Success rate:' + str(int((exito / i)*100) )+ '%')
     logging.info('lista de usuarios creados con error:  ' + str(fail))
 
+    #writing output file
+    raw_data={'Bodytech id':bodytech_ids,'Customer id':customer_ids,'Card id':card_ids}
+    df=pd.DataFrame(raw_data, columns=['Bodytech id', 'Customer id', 'Card id'])
+    output_workbook = 'output.xlsx'
+    df.to_excel(output_workbook, index=False)
